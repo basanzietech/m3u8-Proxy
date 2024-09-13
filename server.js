f3ac5d8b94094d4db87d297d4b684d6ed035b227
Benjamini Omary <167584894+basanzietech@users.noreply.github.com>
2024-09-14 01:31:02 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,13 +2,24 @@ const express = require('express');
 const request = require('request');
 const app = express();
 
-// Proxy endpoint to forward requests with custom headers
 app.get('/proxy', (req, res) => {
-    const videoUrl = req.query.url;
+    const videoUrl = 'https://09-24.mizhls.ru/fls/cdn/mono49/tracks-v1a1/mono.m3u8';
+
     const options = {
         url: videoUrl,
         headers: {
-            'Referer': 'https://your-referer-site.com'
+            'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
+            'sec-ch-ua-mobile': '?0',
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+            'sec-ch-ua-platform': '"Linux"',
+            'Accept': '*/*',
+            'Origin': 'https://1qwebplay.xyz',
+            'Sec-Fetch-Site': 'cross-site',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Dest': 'empty',
+            'Referer': 'https://1qwebplay.xyz/',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'en-US,en;q=0.9'
         }
     };
 
@@ -17,5 +28,5 @@ app.get('/proxy', (req, res) => {
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Proxy server running on port ${port}`);
 });
